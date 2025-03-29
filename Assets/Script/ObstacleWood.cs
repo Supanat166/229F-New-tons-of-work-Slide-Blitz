2d2d0b5b5ade482a18fd48712c35d5ed5519aef0
Supanat166 <supanat.siem@bumail.net>
2025-03-29 17:18:00 +0700

    Update Wood Particle

--- Assets/Script/ObstacleWood.cs
+++ Assets/Script/ObstacleWood.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 public class ObstacleWood : MonoBehaviour, ICanDamage
 {
     [SerializeField] private float health = 20f;
+    [SerializeField] private GameObject hitEffectPrefab; // เปลี่ยนเป็น GameObject
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+
+        if (hitEffectPrefab != null)
+        {
+            Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
+        }
 
         if (health <= 0)
         {
             Destroy(gameObject);
         }
     }
-
 }
--- Assets/Script/Shooter.cs
+++ Assets/Script/Shooter.cs
@@ -3,41 +3,38 @@ using UnityEngine.InputSystem;
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField] private Transform shootPoint;
-    [SerializeField] private GameObject shootPointPrefab;
-    [SerializeField] private GameObject hitPointPrefab;
+    [SerializeField] private Transform shootPoint; 
+    [SerializeField] private GameObject shootPointPrefab; 
     
     void Update()
     {
-        Shooting();
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            Shooting();
+        }
     }
 
     void Shooting()
     {
         RaycastHit hit;
-        
-        Debug.DrawRay(shootPoint.position,  Vector3.forward * 30f, Color.red);
 
-        if (Physics.Raycast(shootPoint.position, Vector3.forward, out hit, 30f))
+        Debug.DrawRay(shootPoint.position, shootPoint.forward * 30f, Color.red);
+
+        if (Physics.Raycast(shootPoint.position, shootPoint.forward, out hit, 30f))
         {
-            Debug.DrawRay(shootPoint.position,  Vector3.forward * hit.distance, Color.green);
-            Debug.Log("hit" + hit.collider.name);
+            Debug.DrawRay(shootPoint.position, shootPoint.forward * hit.distance, Color.green);
+            Debug.Log("Hit: " + hit.collider.name);
             
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                GameObject fireObj = Instantiate(shootPointPrefab, shootPoint.position, Quaternion.identity);
-                GameObject fireObj2 = Instantiate(hitPointPrefab, hit.point, Quaternion.identity);
-                
-                Destroy( fireObj, 1);
-                Destroy( fireObj2, 1);
+            GameObject fireObj = Instantiate(shootPointPrefab, shootPoint.position, Quaternion.identity);
+            Destroy(fireObj, 1);
 
-                ICanDamage damageable = hit.collider.GetComponent<ICanDamage>();
-                if (damageable != null)
-                {
-                    damageable.TakeDamage(10);  
-                }
+            
+            
+            ICanDamage damageable = hit.collider.GetComponent<ICanDamage>();
+            if (damageable != null)
+            {
+                damageable.TakeDamage(10);  
             }
         }
     }
-    
 }
