d31b4f01fbbd2b262e74f90891ab5fed141519f0
Aomaommmmmmmmmm <thamolwan.jomk@bumail.net>
2025-03-30 05:03:14 +0700

    Add ui hp

--- Assets/Script/Hpbar.cs
+++ Assets/Script/Hpbar.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 
-public class Hpbar : MonoBehaviour
+public class HpBar : MonoBehaviour
 {
+    [SerializeField] private Slider sliderHp;
+
+    [SerializeField] private float currentHp;
+
+    [SerializeField] private float maxHp;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentHp = maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHp <= 0)
+        {
+            currentHp = 0;
+            gameObject.SetActive(false); 
+        }
+        sliderHp.value = currentHp / maxHp;
+    }
+    public void takeDamage(float damageAmount)
+
+    {
+        currentHp -= damageAmount;
+        sliderHp.value = currentHp / maxHp;
     }
 }
